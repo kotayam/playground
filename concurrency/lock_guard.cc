649008018f7b2b7ce1b77312d9a7fa6d4051f4ef
kotayam <ykomako4.9@gmail.com>
2026-01-13 23:49:00 -0500

    join threads

--- concurrency/lock_guard.cc
+++ concurrency/lock_guard.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 volatile int counter = 0;
 std::mutex counter_mutex;
@@ -12,8 +13,19 @@ void increment(int id) {
 }
 
 int main() {
+  std::vector<std::thread> threads;
   for (int i = 0; i < 10; i++) {
-    std::thread(&increment, i);
+    threads.push_back(std::thread(&increment, i));
   }
+
+  // wait for threads to finish
+  for (auto &t : threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+
+  std::cout << "Final counter: " << counter << std::endl;
+
   return 0;
 }
