aa60b50a178f318af0aba234977a5fb1c68d9162
kotayam <ykomako4.9@gmail.com>
2026-01-16 12:02:59 -0500

    fix unique lock example

--- concurrency/unique_lock.cc
+++ concurrency/unique_lock.cc
@@ -5,21 +5,21 @@
 
 #define NUM_THREADS 10
 
-int counter = 0;
+volatile int counter = 0;
 std::mutex counter_mutex;
 
 void increment(int id) {
-  std::unique_lock<std::mutex> lock(counter_mutex);
+  std::unique_lock<std::mutex> lock(counter_mutex, std::defer_lock);
   lock.lock();
   counter++;
-  lock.unlock();
   std::cout << id << ": counter = " << counter << std::endl;
+  lock.unlock();
 }
 
 int main() {
   std::vector<std::thread> threads;
 
-  for (int i = 0; NUM_THREADS; i++) {
+  for (int i = 0; i < NUM_THREADS; i++) {
     threads.emplace_back(&increment, i);
   }
 
